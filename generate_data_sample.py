ae7582f72149bd9044bd7a5e64893fc5194ac80b
danPr0 <danPr080704@gmail.com>
2025-11-09 23:49:18 +0200

    feat: app_activity data cleanup

--- generate_data_sample.py
+++ generate_data_sample.py
@@ -1,8 +1,23 @@
 import pandas as pd
 
 
-first_chunk = True
-for chunk in pd.read_csv('data/initial/APP_ACTIVITY.csv', chunksize=100_000):
-    sample = chunk.sample(frac=0.05)
-    sample.to_csv('data/samples/APP_ACTIVITY_SAMPLE.csv', mode='a', index=False, header=first_chunk)
-    first_chunk = False
+# df = pd.read_csv('data/initial/CLIENTS.csv')
+# df_sample = df.sample(frac=0.05)
+# df_sample.to_csv('data/samples/CLIENTS_SAMPLE.csv', index=False)
+
+
+# IN_FILENAME = 'data/initial/TRANSACTIONS.csv'
+# OUT_FILENAME = 'data/samples/TRANSACTIONS_SAMPLE.csv'
+# IN_FILENAME = 'data/initial/APP_ACTIVITY.csv'
+# OUT_FILENAME = 'data/samples/APP_ACTIVITY_SAMPLE.csv'
+IN_FILENAME = 'data/initial/COMMUNICATIONS.csv'
+OUT_FILENAME = 'data/samples/COMMUNICATIONS_SAMPLE.csv'
+
+
+client_ids = pd.read_csv('data/samples/CLIENTS_SAMPLE.csv')['CLIENT_ID'].values
+df = pd.read_csv(IN_FILENAME)
+res = pd.DataFrame()
+for client_id, groups in df.groupby('CLIENT_ID'):
+    if client_id in client_ids:
+        res = pd.concat([res, groups], ignore_index=True)
+res.to_csv(OUT_FILENAME, index=False)
